d38182a93499bb618a9d153bca485987b8215fb2
damiankieltyka <damian.kieltyka@icloud.com>
2019-10-06 20:46:33 +0200

    add google credentials

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '1032151717013-e9sfma2usds84u60pkim23699vhj7hg7.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'Cmv1j5BdLOKBlhG4lhXZmZWI',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
